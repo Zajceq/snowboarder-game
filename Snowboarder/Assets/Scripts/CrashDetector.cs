1110faa545fe207f9075de498839193445c6cca2
Zajceq <l.gebicz@gmail.com>
2022-07-04 00:22:43 +0200

    Add snowboarder game

---
+++ Snowboarder/Assets/Scripts/CrashDetector.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CrashDetector : MonoBehaviour
+{
+    [SerializeField] float loadDelay = 0.5f;
+    [SerializeField] ParticleSystem crashEffect;
+    [SerializeField] AudioClip crashSFX;
+    CircleCollider2D playerHead;
+    bool hasCrashed = false;
+    void Start()
+    {
+        playerHead = GetComponent<CircleCollider2D>();
+    }  
+ 
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Ground" && playerHead.IsTouching(other.collider) && hasCrashed == false)
+        {
+            hasCrashed = true;
+            FindObjectOfType<PlayerController>().DisableControls();
+            crashEffect.Play();
+            GetComponent<AudioSource>().PlayOneShot(crashSFX);
+            Invoke("ReloadScene", loadDelay);
+        }
+    }
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
